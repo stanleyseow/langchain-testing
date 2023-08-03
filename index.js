6f1db8c1eeb76be78a980650ce2e25e1029d882a
Stanley Seow <stanleyseow@gmail.com>
2023-08-03 22:58:54 +0800

    Added express

--- index.js
+++ index.js
@@ -7,57 +7,71 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
-let arg = process.argv.slice(2)
-const str = arg.join(' ');
-console.log("Question: ", str);
+import express from 'express'
 
-// 2. Load environment variables
 dotenv.config();
 
-// 3. Set up input data Sand paths
-const txtFilename = "AI_Superpower";
-let question = "Tell me about The techno-optimists";
+const txtFilename = "AI_Superpowers";
+//const question = "Tell me about “The Experimenter and the Finisher";
+const question = "Who purchased kaixin ?";
 const txtPath = `./${txtFilename}.txt`;
+
 const VECTOR_STORE_PATH = `${txtFilename}.index`;
 
-question = str
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5056;
+
+app.post("/ask", async (req, res) => {
+    const question = req.body.prompt;
+    let result = await runWithEmbeddings(question);
+
+    console.log(result.text);
+
+    return res.status(200).json({
+        success: true,
+        message: result.text,
+    });
+
+
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}!!`));
 
 // 4. Define the main function runWithEmbeddings
-export const runWithEmbeddings = async () => {
-  // 5. Initialize the OpenAI model with an empty configuration object
-  const model = new OpenAI({});
+async function runWithEmbeddings(question) {
+    // 5. Initialize the OpenAI model with an empty configuration object
+    const model = new OpenAI({});
 
-  // 6. Check if the vector store file exists
-  let vectorStore;
-  if (fs.existsSync(VECTOR_STORE_PATH)) {
-    // 6.1. If the vector store file exists, load it into memory
-    console.log('Vector Exists..');
-    vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
-  } else {
-    // 6.2. If the vector store file doesn't exist, create it
-    // 6.2.1. Read the input text file
-    const text = fs.readFileSync(txtPath, 'utf8');
-    // 6.2.2. Create a RecursiveCharacterTextSplitter with a specified chunk size
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-    // 6.2.3. Split the input text into documents
-    const docs = await textSplitter.createDocuments([text]);
-    // 6.2.4. Create a new vector store from the documents using OpenAIEmbeddings
-    vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-    // 6.2.5. Save the vector store to a file
-    await vectorStore.save(VECTOR_STORE_PATH);
-  }
+    // 6. Check if the vector store file exists
+    let vectorStore;
+    if (fs.existsSync(VECTOR_STORE_PATH)) {
+        // 6.1. If the vector store file exists, load it into memory
+        console.log('Vector Exists..');
+        vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
+    } else {
+        // 6.2. If the vector store file doesn't exist, create it
+        // 6.2.1. Read the input text file
+        const text = fs.readFileSync(txtPath, 'utf8');
+        // 6.2.2. Create a RecursiveCharacterTextSplitter with a specified chunk size
+        const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+        // 6.2.3. Split the input text into documents
+        const docs = await textSplitter.createDocuments([text]);
+        // 6.2.4. Create a new vector store from the documents using OpenAIEmbeddings
+        vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+        // 6.2.5. Save the vector store to a file
+        await vectorStore.save(VECTOR_STORE_PATH);
+    }
 
-  // 7. Create a RetrievalQAChain by passing the initialized OpenAI model and the vector store retriever
-  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    // 7. Create a RetrievalQAChain by passing the initialized OpenAI model and the vector store retriever
+    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
 
-  // 8. Call the RetrievalQAChain with the input question, and store the result in the 'res' variable
-  const res = await chain.call({
-    query: question,
-  });
+    // 8. Call the RetrievalQAChain with the input question, and store the result in the 'res' variable
+    const res = await chain.call({
+        query: question,
+    });
 
-  // 9. Log the result to the console
-  console.log({ res });
+    // 9. Log the result to the console
+    //console.log({ res });
+    return res;
 };
-
-// 10. Execute the main function runWithEmbeddings
-runWithEmbeddings();
